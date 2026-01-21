bae48fc9ff004e835e3f9438a53ff012f94095d1
zeynepcamgoz <Zeynep.Camgoz@edu.sait.ca>
2026-01-21 16:56:00 -0700

    Finish

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Zeynep Camgoz</p>
+      <Link href="https://github.com/zeynepcamgoz/cprg306-assignments">
+        Github: zeynepcamgoz/cprg306-assignments
+      </Link>
+    </div>
+
+
+
+  );
+}
---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./StudentInfo";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo></StudentInfo>
+    </main>
+  );
+}
