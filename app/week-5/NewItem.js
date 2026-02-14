072f619dac51752765b01223f5de9cd9ffd7a5fa
zeynepcamgoz <Zeynep.Camgoz@edu.sait.ca>
2026-02-13 21:40:33 -0700

    Final with all styling correct

--- app/week-5/NewItem.js
+++ app/week-5/NewItem.js
@@ -50,40 +50,42 @@ export default function NewItem() {
             className="rounded-md border border-pink-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 p-2 outline-none transition text-black"
           />
         </div>
-        <div className="flex flex-col">
-          <label className="text-pink-700 font-medium mb-1">
-            Quantity:
-          </label>
-          <input
-            type="number"
-            min="1"
-            max="99"
-            value={quantity}
-            onChange={updateQuantity}
-            className="border border-pink-300 rounded-md px-3 py-2 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 outline-none transition text-black"
-          />
-        </div>
-        <div className="flex flex-col">
-          <label className="text-pink-700 font-medium mb-1">
-            Category:
-          </label>
-          <select
-            value={category}
-            onChange={updateCategory}
-            className="border border-pink-300 rounded-md px-3 py-2 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 outline-none transition text-black"
-          >
-            <option>Produce</option>
-            <option>Dairy</option>
-            <option>Bakery</option>
-            <option>Meat</option>
-            <option>Frozen Foods</option>
-            <option>Canned Goods</option>
-            <option>Dry Goods</option>
-            <option>Beverages</option>
-            <option>Snacks</option>
-            <option>Household</option>
-            <option>Other</option>
-          </select>
+        <div className="flex">
+          <div className="flex flex-row mr-2">
+            <label className="text-pink-700 font-medium mb-1">
+              Quantity:
+            </label>
+            <input
+              type="number"
+              min="1"
+              max="99"
+              value={quantity}
+              onChange={updateQuantity}
+              className="border border-pink-300 rounded-md px-3 py-2 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 outline-none transition text-black"
+            />
+          </div>
+          <div className="flex flex-row">
+            <label className="text-pink-700 font-medium mb-1">
+              Category:
+            </label>
+            <select
+              value={category}
+              onChange={updateCategory}
+              className="border border-pink-300 rounded-md px-3 py-2 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 outline-none transition text-black"
+            >
+              <option>Produce</option>
+              <option>Dairy</option>
+              <option>Bakery</option>
+              <option>Meat</option>
+              <option>Frozen Foods</option>
+              <option>Canned Goods</option>
+              <option>Dry Goods</option>
+              <option>Beverages</option>
+              <option>Snacks</option>
+              <option>Household</option>
+              <option>Other</option>
+            </select>
+          </div>
         </div>
         <div className="flex justify-center pt-4">
           <button
