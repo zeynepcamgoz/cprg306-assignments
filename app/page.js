70afa3419cb0ef3b3d01b52238c0d1366b9177a5
zeynepcamgoz <Zeynep.Camgoz@edu.sait.ca>
2026-02-13 21:36:12 -0700

    removed the random dash

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <h1>CPRG306: Web Development 2 - Assignments</h1>
       <Link href="/week-2">week-2</Link><br></br>
       <Link href="/week-3">week-3</Link><br></br>
-      <Link href="/week-4">week-4</Link>|<br></br>
+      <Link href="/week-4">week-4</Link><br></br>
       <Link href="/week-5">week-5</Link>
     </main>
   );
